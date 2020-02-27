ca21e9a61ce274cc8298056ebb6911be3ebb95bb
EYoung75 <eyoung75.ey@gmail.com>
2020-02-27 10:40:42 -0600

    playing around with secondary design, starting from mobile

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,34 +1,60 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import { FaFacebookSquare, FaGooglePlusSquare } from "react-icons/fa";
 
-const Header = () => {
-  return <div className="header">
-      <h2 className="header__logo">EY20'</h2>
-      <ul className="header__navigation">
-        <Link
-          to="/"
-          exact
-          className="header__navigation__link header__navigation__link-1"
-          activeClassName="header__navigation__link--active"
-        >
-          <li>Home</li>
-        </Link>
-        <Link
-          to="/work"
-          className="header__navigation__link header__navigation__link-2"
-          activeClassName="header__navigation__link--active"
-        >
-          <li>Work</li>
-        </Link>
-        <Link
-          to="/about"
-          className="header__navigation__link header__navigation__link-3"
-          activeClassName="header__navigation__link--active"
-        >
-          <li>About</li>
-        </Link>
-      </ul>
-  </div>;
-};
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      open: false
+    };
+  }
+
+  toggleNav = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  render() {
+    return (
+      // <nav className="header">
+      //   <h2 className="header__logo">EY20'</h2>
+      //   <ul className="header__navigation">
+      //     <Link
+      //       to="/"
+      //       exact
+      //       className="header__navigation__link header__navigation__link-1"
+      //       activeClassName="header__navigation__link--active"
+      //     >
+      //       <li>Home</li>
+      //     </Link>
+      //     <Link
+      //       to="/work"
+      //       className="header__navigation__link header__navigation__link-2"
+      //       activeClassName="header__navigation__link--active"
+      //     >
+      //       <li>Work</li>
+      //     </Link>
+      //     <Link
+      //       to="/about"
+      //       className="header__navigation__link header__navigation__link-3"
+      //       activeClassName="header__navigation__link--active"
+      //     >
+      //       <li>About</li>
+      //     </Link>
+      //   </ul>
+      // </nav>
+      <nav
+        className={"navigation " + (this.state.open ? "navigation-open" : "")}
+      >
+        <div className="button-background" onClick={this.toggleNav}>
+          <div
+            className={this.state.open ? "button button-close" : "button"}
+          ></div>
+          
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default Header;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,16 +4,26 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="home">
-      <div className="home__bio">
-        <img src={require("../assets/me.jpg")} alt="Picture of yours truly" className="home__image"/>
-        <p>
-          Hey there. My name is Evan Young and this is my portfolio site. If you
-          don't really know what the hell is going on here, that makes two. But
-          kick off your shoes or something and make yourself comfortable. I'm an avid
-          adventurer, recent Kansas City transplant, and problem solving
-          enthusiast. My passion lies in creating elegant coding solutions for
-          complex, real world problems.
-        </p>
+      <div className="home__hero">
+        <div className="home__hero__title">
+          <h1>EY 20'</h1>
+          <h2>web development portfolio</h2>
+        </div>
+      </div>
+      <div className="home__sub">
+        <div className="home__sub__content">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+          soluta accusamus ratione, quisquam pariatur perferendis odit
+          dignissimos porro? Culpa quas dolorum deleniti minus recusandae!
+          Explicabo doloremque error impedit saepe dolor.
+        </div>
+        <div className="home__sub__pic" />
+        <div className="home__sub__content home__sub__content-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, quo
+          numquam. Aperiam quibusdam deserunt id, natus ut itaque maxime odio
+          deleniti dolorum molestias corrupti veniam adipisci ex, officiis
+          reiciendis laudantium!
+        </div>
       </div>
     </div>
   );
