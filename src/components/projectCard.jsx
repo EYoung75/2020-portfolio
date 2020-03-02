8ea9669630f1ce486d298aa70e5708da0de7b547
EYoung75 <eyoung75.ey@gmail.com>
2020-03-02 12:04:41 -0600

    added projects map and replaced placeholder content

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -1,31 +1,30 @@
 import React from "react";
 import { FaGithubAlt, FaGlobe } from "react-icons/fa";
 
-const ProjectCard = () => {
+const ProjectCard = props => {
+  let project = props.props;
   return (
     <div className="project">
       <div className="project__title">
-        <h3>Project Title</h3>
+        <h3>{project.name}</h3>
       </div>
       <div className="project__preview">
-        <img src={require("../assets/me.jpg")} />
+        <img src={project.img} />
       </div>
       <div className="project__links">
-        <a href="#" target="_blank">
-          <FaGithubAlt/> {"<Code/>"}
+        <a href={project.github} target="_blank">
+          <FaGithubAlt /> {"<Code/>"}
         </a>
-        <a href="#" target="_blank">
-          <FaGlobe/> Live Site
-        </a>
+        {project.live === "" ? (
+          ""
+        ) : (
+          <a href={project.live} target="_blank">
+            <FaGlobe /> Live Site
+          </a>
+        )}
       </div>
       <div className="project__description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quos quod
-        aperiam harum mollitia omnis iusto doloribus enim. Ex totam aut deleniti
-        ut quidem molestias necessitatibus praesentium dolores quo quas. Lorem
-        ipsum dolor, sit amet consectetur adipisicing elit. Molestiae ea neque
-        vitae odit in incidunt magni, veniam, quod atque rerum aliquam iusto
-        veritatis maiores. Eligendi consequuntur delectus quod soluta
-        necessitatibus!
+        <p>{project.description}</p>
       </div>
     </div>
   );
--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -2,14 +2,44 @@ import React from "react";
 import ProjectCard from "../components/projectCard.jsx";
 
 const Work = () => {
+  const projects = [
+    {
+      name: "OneCard",
+      img: require("../assets/onecard.gif"),
+      description:
+        "A 'Virtual Business Card' sharing app built using Flutter and implementing user-authentication and various Firebase services on the backend. Revolves around the user specific functionality of creating a virtual business card and scanning QR codes to share and add to a virtual wallet. Implements full CRUD functionality.",
+      github: "https://github.com/EYoung75/flutter_oneCard",
+      live: ""
+    },
+    {
+      name: "PinPoint",
+      img: require("../assets/pinpoint.gif"),
+      description: "A Flutter place searching application allowing users to search and view details for hot spots and places of interest around them. Provides streamlined data populated from Google's Places and Maps API in conjunction with Yelp's API for review data.",
+      github: "https://github.com/EYoung75/pinpoint_flutter",
+      live: ""
+    },
+    {
+      name: "Movie Reviews",
+      img: require("../assets/movie-reviews.gif"),
+      description: "A full CRUD React app that allows users to review movies. Utilizes a back-end created using Express.js and PostgreSQL.",
+      github: "https://github.com/EYoung75/movie-reviews",
+      live: "http://roomy-apparatus.surge.sh/"
+    },
+    // {
+    //   name: "Something else",
+    //   img: require("../assets/movie-reviews.gif"),
+    //   description: "",
+    //   github: "",
+    //   live: ""
+    // }
+  ];
   return (
     <div className="work">
       <h2 className="work__title">Latest Projects:</h2>
       <div className="work__projects">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {projects.map(project => (
+          <ProjectCard props={project} />
+        ))}
       </div>
     </div>
   );
