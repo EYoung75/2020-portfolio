bfe6d6929b7b3517651475f5fee50ac5decf391a
EYoung75 <eyoung75.ey@gmail.com>
2020-04-08 16:42:42 -0500

    refined home banner styling

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,14 +3,13 @@ import {
   FaGithubAlt,
   FaLinkedin,
   FaEnvelope,
-  FaCameraRetro
+  FaCameraRetro,
 } from "react-icons/fa";
 import "./work.jsx";
 import Work from "./work.jsx";
 import About from "./about.jsx";
 import Contact from "./contact.jsx";
 import Resume from "../components/resume.jsx";
-
 
 const Home = () => {
   return (
@@ -21,32 +20,45 @@ const Home = () => {
           <div className="home__hero__banner">
             <h2>Software Developer {"&"} avid adventurer</h2>
             <p>
-              With a passion for crafting engaging user experiences and coding solutions
+              With a passion for crafting engaging user experiences and coding
+              solutions
             </p>
           </div>
           {/* <div className="home__hero__banner__scroll">
             <p>scroll</p>&darr;
           </div> */}
           <div className="home__hero__footer">
-            <a href="https://github.com/EYoung75/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/EYoung75/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaGithubAlt />
             </a>
-            <a href="https://www.linkedin.com/in/evanyoung4/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.linkedin.com/in/evanyoung4/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaLinkedin />
             </a>
             <a href="mailto:young.evanj@gmail.com" target="_top">
               <FaEnvelope />
             </a>
-            <a href="https://vsco.co/evanyoung75/gallery" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://vsco.co/evanyoung75/gallery"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaCameraRetro />
             </a>
           </div>
         </div>
       </div>
       <Work />
-      <About/>
-      <Resume/>
-      <Contact/>
+      <About />
+      <Resume />
+      <Contact />
     </div>
   );
 };
