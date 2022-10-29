90b55c1a095dc72c20a7d62c4d942bf4502f8c4e
EYoung75 <eyoung75.ey@gmail.com>
2022-10-28 22:50:34 -0500

    Updated bio, omitted misc. project

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -5,22 +5,25 @@ const About = () => {
   return (
     <div className="about">
       {/* <Logo /> */}
-      <h1>About</h1>
+      <h1>Hey, I'm Evan</h1>
       <div className="about__bio">
-        <p>I'm a Software Engineer and recent Kansas City transplant.</p>
-        <p>•••</p>
+        {/* <p>I'm a Software Engineer and recent Kansas City transplant.</p>
+        <p>•••</p> */}
         <p>
-          Before taking on the role of software engineer, I have worked various jobs
-          ranging from a deckhand out of Juneau, Alaska to serving in the United
-          States Navy. This eclectic background provided me with the skills to
-          function as an effective team player and the ability to approach
-          problems through the lens of a diverse problem solver. I love taking
-          on new challenges and engaging in new and exciting experiences.
+          My career in tech began shortly after leaving the United States Navy back in 2018,
+          when I attended the Immersive Software Engineering Program at Galvanize - Denver Campus.
+          Fast forward several years and I now have the opportunity to make big ideas happen on a daily basis.
         </p>
         <p>••• </p>
         <p>
-          I spend my free time exploring my new home in KC with my dog Maximus,
-          picking up on a new hobby, or broadening my technological skillset.
+          I have experience working within several different industries and with various team sizes and dynamics.    
+          I love collaborating to tackle real world problems and enjoy writing code that is performant, maintainable, and well documented.
+          I'm an optimization enthusiast with a passion for continuous learning and growth, and I strongly believe that no problem is too hard if you break it down into smaller pieces.
+        </p>
+        <p>••• </p>
+        <p>
+          When I'm not coding I enjoy traveling, spending time with friends (and/or my dog Max), playing guitar, photography, marathon/triathlon, and tinkering with new hobbies.
+          Check out my GitHub for more work and feel free to connect with me on LinkedIn.
         </p>
       </div>
     </div>
